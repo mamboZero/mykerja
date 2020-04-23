018a0bd2a717e863c0a7a334a1aa39fa550e808f
Haizul <proxy.shadowing@gmail.com>
2020-04-23 19:43:40 +0800

    task features

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,19 @@ class Task < ApplicationRecord
 	belongs_to :user
 
 	validates :no_fon, length: {minimum: 5 , maximum: 12, message: "Adakah Anda Tersalah Memasukkan No. Telefon?"}
+	validates :email_client, presence: true
+
+	def num_ticket_nil
+		if no_tix == nil
+			"Tiada"
+		else
+			no_tix
+		end
+	end
+
+	def two_char
+		website_name.slice(0...2)
+	end
 
 
 	def mys_num
