adccc23e8f0ed1879a2f5b693e77314b13fdeb9f
Haizul <proxy.shadowing@gmail.com>
2020-05-04 02:31:59 +0800

    assets clean

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require('jquery')
-require('packs/main')
-require('packs/animsition.min')
-require('packs/bootstrap.min')
-require('packs/daterangepicker')
-require('packs/moment.min')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
